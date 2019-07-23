4bd3bab8e3163ccc79435d90e6e3d4c8d109d2ab
Mangocoin <52526923+mangocoinproject@users.noreply.github.com>
2019-07-23 12:57:11 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-        'api' => 'http://3.18.105.111:11898',
+        'api' => 'http://13.58.16.6:11898',
         'blockTargetInterval' => 120,
         'coinUnits' => 100000000
 );
