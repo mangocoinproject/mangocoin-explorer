36524edcb4ad80c2d88b36947d94369b4a946196
Mangocoin <52526923+mangocoinproject@users.noreply.github.com>
2019-08-27 14:01:09 -0400

    Update index.php

--- q/hashrate/index.php
+++ q/hashrate/index.php
@@ -1,10 +1,14 @@
-<?php
-require '../util.php';
-$config = (require '../../config.php');
-
-$info = fetch_getinfo($config['api']);
-
-$difficulty = $info['difficulty'];
-$hashrate = round($difficulty / $config['blockTargetInterval']);
+<?php 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, 'http://pool.mangocoin.online:11898/getinfo');
+$result = curl_exec($ch);
+$obj = json_decode($result, TRUE);
+curl_close($ch);
+//print_r($obj);
+$difficulty = $obj['difficulty'];
+$hashrate = round($difficulty / 30);
 
 print_r($hashrate);
+?>
