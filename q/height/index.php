dbd7e62223eb55676cf234f55820e801c1ca3a41
Mangocoin <52526923+mangocoinproject@users.noreply.github.com>
2019-08-27 14:01:56 -0400

    Update index.php

--- q/height/index.php
+++ q/height/index.php
@@ -1,7 +1,10 @@
-<?php
-require '../util.php';
-$config = (require '../../config.php');
-
-$info = fetch_getinfo($config['api']);
-
-print_r($info['last_known_block_index']);
+<?php 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, 'http://pool.mangocoin.online:11898/getinfo');
+$result = curl_exec($ch);
+$obj = json_decode($result, TRUE);
+curl_close($ch);
+print_r($obj['last_known_block_index']);
+?>
