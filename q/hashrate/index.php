82cc2f9ddada4655a84bac135a36819597decb70
mangocoinproject <52526923+mangocoinproject@users.noreply.github.com>
2019-08-29 16:51:33 -0400

    Update

--- q/hashrate/index.php
+++ q/hashrate/index.php
@@ -1,14 +1,11 @@
-<?php 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, 'http://pool.mangocoin.online:11898/getinfo');
-$result = curl_exec($ch);
-$obj = json_decode($result, TRUE);
-curl_close($ch);
-//print_r($obj);
-$difficulty = $obj['difficulty'];
-$hashrate = round($difficulty / 30);
+<?php
+require dirname(__FILE__) . '/../util.php';
+$config = (require dirname(__FILE__) . '/../../config.php');
+
+$info = fetch_getinfo($config['api']);
+
+$difficulty = $info['difficulty'];
+$hashrate = round($difficulty / $config['blockTargetInterval']);
 
 print_r($hashrate);
 ?>
--- q/height/index.php
+++ q/height/index.php
@@ -1,10 +1,9 @@
-<?php 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, 'http://pool.mangocoin.online:11898/getinfo');
-$result = curl_exec($ch);
-$obj = json_decode($result, TRUE);
-curl_close($ch);
-print_r($obj['last_known_block_index']);
+<?php
+require dirname(__FILE__) . '/../util.php';
+$config = (require dirname(__FILE__) . '/../../config.php');
+
+$info = fetch_getinfo($config['api']);
+
+print_r($info['last_known_block_index']);
+
 ?>
