4abcaa280fda7fba4ffaee6d12e757b0ebd3967b
Mangocoin <52526923+mangocoinproject@users.noreply.github.com>
2019-09-06 19:32:44 -0400

    Update index.php

--- q/supply/index.php
+++ q/supply/index.php
@@ -8,7 +8,7 @@ $hash = $hashData['result']['block_header']['hash'];
 $blockData = url_get_contents_post($config['api'], 'f_block_json', '{"hash":"'.$hash.'"}');
 $supplyRaw = $blockData['result']['block']['alreadyGeneratedCoins'];
 
-$supply = number_format($supplyRaw / $config['coinUnits'], 2, ".", "");
+$supply = number_format($supplyRaw / $config['coinUnits'], 8, ".", "");
 
 print_r($supply);
 ?>
